39be460a5e09e3a653dcb448c05404c143216031
Hariom Chouhan <75517242+hariomchouhan@users.noreply.github.com>
2024-04-04 04:38:59 +0000

    variables complete

---
+++ JS_Codes/01_Variables.js
@@ -0,0 +1,10 @@
+const accountId = 87688;
+let accountName = "SBI";
+var accountNumber = 9878787249857;
+
+accountEmail = "hariomchouhan430@gmail.com";
+accountPassword = 123456;
+
+console.log(accountId);
+
+console.table([accountId, accountName, accountNumber, accountEmail, accountPassword]);
