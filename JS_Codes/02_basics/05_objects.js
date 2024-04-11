f3f01a4d78796c4c540ee27715756eaacf2b21f7
Hariom Chouhan <75517242+hariomchouhan@users.noreply.github.com>
2024-04-11 06:14:40 +0000

    Object de structuring and JSON API intro

---
+++ JS_Codes/02_basics/05_objects.js
@@ -0,0 +1,13 @@
+const course = {
+    courseName: "js in hindi",
+    price: "999",
+    courseInstructor: "Hariom",
+}
+
+// course.courseInstructor
+
+// const {courseInstructor} = course;
+const {courseInstructor: instructor} = course;
+
+// console.log(courseInstructor);
+console.log(instructor);
