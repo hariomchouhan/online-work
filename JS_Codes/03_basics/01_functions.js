eb25c1ea14c9209d7ffdd99f524a862701d3c09b
Hariom Chouhan <75517242+hariomchouhan@users.noreply.github.com>
2024-04-16 05:18:23 +0000

    add one example of rest operator

--- JS_Codes/03_basics/01_functions.js
+++ JS_Codes/03_basics/01_functions.js
@@ -13,23 +13,22 @@ function sayMyName() {
 //     console.log(number1 + number2);
 // }
 function addTwoNumbers(number1, number2) {
-    // let result = number1 + number2
-    // return result
+  // let result = number1 + number2
+  // return result
 
-    return number1 + number2
+  return number1 + number2;
 }
 
-const result = addTwoNumbers(3,5)
+const result = addTwoNumbers(3, 5);
 
 // console.log("Result: ", result);
 
-
 function loginUserMessage(username = "Hariom") {
   // if(username === undefined) {
-  if(!username) {
+  if (!username) {
     console.log("Please enter a username");
   }
-  return `${username} just logged in`
+  return `${username} just logged in`;
 }
 
 // console.log(loginUserMessage("Hariom"))
@@ -37,7 +36,7 @@ function loginUserMessage(username = "Hariom") {
 
 // Putting ... in the parameter of a function is called rest operator.
 function calculateCartPrice(val1, val2, ...nums) {
-  return nums
+  return nums;
 }
 
 // console.log(calculateCartPrice(200, 400, 500, 2000))
@@ -45,24 +44,38 @@ function calculateCartPrice(val1, val2, ...nums) {
 const user = {
   username: "hariom",
   // price: 999
-  prices: 999
-}
+  prices: 999,
+};
 
 function handleObject(anyObject) {
-  console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+  console.log(
+    `Username is ${anyObject.username} and price is ${anyObject.price}`
+  );
 }
 
 // handleObject(user)
 // handleObject({
-//   username: "Varsha",
+//   username: "Hariom",
 //   price: 999
 // })
 
-const myNewArray = [200, 300, 400, 500]
+const myNewArray = [200, 300, 400, 500];
 
 function returnSecondValue(getArray) {
-  return getArray[1]
+  return getArray[1];
 }
 
 // console.log(returnSecondValue(myNewArray))
-console.log(returnSecondValue([200, 400, 500, 660]))
+console.log(returnSecondValue([200, 400, 500, 660]));
+
+
+
+
+
+
+
+// function sum(...numbers) {
+//   return numbers.reduce((total, num) => total + num, 0);
+// }
+
+// console.log("Line number 81: ",sum(200, 300, 400, 500))
