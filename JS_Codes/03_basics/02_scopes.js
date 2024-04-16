cb376e89e9f3f107be91c4ff09f200e6a49151e9
Hariom Chouhan <75517242+hariomchouhan@users.noreply.github.com>
2024-04-16 06:10:16 +0000

    Scope level and mini hoisting in javascript

--- JS_Codes/03_basics/02_scopes.js
+++ JS_Codes/03_basics/02_scopes.js
@@ -6,10 +6,51 @@ if(true) {
     let a = 10
     const b = 20
     var c = 30
-    console.log("Inside: ", a);
+    // console.log("Inside: ", a);
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+function one() {
+    const username = "Hariom"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if(true) {
+    const username = "Hariom"
+    if (true) {
+        const surname = "Chouhan"
+        // console.log(username + " " + surname);
+    }
+    // console.log(surname);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+addOne(5) // execute the function using hoisting
+function addOne(num) {
+    return num + 1
+}
+
+
+// addTwo(8) // ReferenceError: Cannot access 'addTwo' before initialization
+const addTwo = function (num) {
+    return num + 2
+}
